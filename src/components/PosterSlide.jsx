cc80329e07a282ffe9a8f46253f07025e3b4a49a
StanleyFerreira <stanley_ferreira_@outlook.com>
2023-08-11 11:12:39 -0300

    commit

--- src/components/PosterSlide.jsx
+++ src/components/PosterSlide.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import tmdbConfigs from "../config/tmdb.configs";
@@ -58,65 +58,4 @@ const PosterSlide = ({movie}) => {
   );
 };
 
-=======
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import tmdbConfigs from "../config/tmdb.configs";
-import YouTube from "react-youtube"
-
-import ProgressCircle from "./ProgressCircle";
-
-const imagesURL = import.meta.env.VITE_IMG;
-const apiKey = import.meta.env.VITE_API_KEY;
-const backURL = import.meta.env.VITE_BACK_IMG;
-import "./style/MoviesGrid.css";
-import "./style/Backdrop.css";
-
-
-
-
-const PosterSlide = ({movie}) => {
-
-  const maxLength = 100;
-  let viewLimit = movie.overview.slice(0, maxLength);
-  
-    if (movie.overview.length > maxLength) {
-      const lastSpaceIndex = movie.overview.lastIndexOf(' ', maxLength);
-      viewLimit = movie.overview.slice(0, lastSpaceIndex) + '...';
-    }
-
-
-
-  return (
-    <div className="poster-box">
-    <div className="poster-card" style={{
-        backgroundImage: `
-        linear-gradient(to right, rgba(0,0,0,1) -100%, rgba(0,0,0,0)),
-        url(${backURL + movie.backdrop_path || movie.poster_path})`,
-      }}>
-
-      <Link className="info-poster" to={`/${movie.media_type}/${movie.id}`} >
-        <div className="info-details-poster">
-        <h2 id="poster-title">{movie.title || movie.name}</h2>
-
-
-        <div className="infoPoster">
-        <div className="voteGenres">
-        < ProgressCircle className="progress" percent={movie.vote_average * 10}/>
-        <div className="trailer">
-        <ion-icon id="iconTrailer" name="caret-forward-outline"></ion-icon>
-        </div>
-        </div>
-        <p>{viewLimit}</p>
-        </div>
-        </div>
-        <p>
-        </p>
-      </Link>
-    </div>
-    </div>
-  );
-};
-
->>>>>>> 437ce868e3fac8517dcd36f7361c6dd2f3200194
 export default PosterSlide;
--- src/components/TvCard.jsx
+++ src/components/TvCard.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Link } from "react-router-dom";
 
 import { FaStar } from "react-icons/fa";
@@ -35,41 +35,3 @@ const TvCard = ({series}) => {
 
 
 export default TvCard;
-=======
-import { Link } from "react-router-dom";
-
-import { FaStar } from "react-icons/fa";
-
-const imagesURL = import.meta.env.VITE_IMG;
-const apiKey = import.meta.env.VITE_API_KEY;
-import tmdbConfigs from "../config/tmdb.configs";
-import ProgressCircle from "./ProgressCircle";
-
-import "./style/MoviesGrid.css";
-
-
-
-const TvCard = ({series}) => {
-
-
-  return (
-    <div className="movie-card">
-
-      <Link to={`/${tmdbConfigs.mediaType.tv}/${series.id}`}>
-
-      <div id="banner" style={{ backgroundImage: `url(${imagesURL + series.poster_path})`,  }}></div>
-        <div className="Nota">
-        <ProgressCircle percent={series.vote_average * 10} />
-      </div>
-        <h2 id="title">{series.name}</h2>
-        <p>
-        </p>
-      </Link>
-    </div>
-  );
-
-};
-
-
-export default TvCard;
->>>>>>> 437ce868e3fac8517dcd36f7361c6dd2f3200194
